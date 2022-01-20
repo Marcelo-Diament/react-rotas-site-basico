2c99c97a380750d3c4a2b8748290bf699dbd6309
Marcelo Diament <marcelo.diament@driven.cx>
2022-01-19 21:21:08 -0300

    feature: footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <h1 className="footer__title">Footer</h1>
+      <p className="footer__description">Indústrias Tabajara &copy;</p>
+    </section>
+  );
+};
+
+export default Footer;
