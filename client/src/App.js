e236fae58cf3175bddef06f6fa442e93360f976a
Marcelo Diament <marcelo.diament@driven.cx>
2022-01-19 21:57:12 -0300

    feature: app updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Routes from "./routes";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const lightAsDefault =
+    new Date().getHours() > 8 && new Date().getHours() < 18;
+
+  const [isLight, setIsLight] = useState(lightAsDefault);
+
+  const toggleTheme = () => setIsLight(!isLight);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App${!isLight ? " dark" : ""}`}>
+      <Router>
+        <Header themeClick={toggleTheme} />
+        <Routes />
+        <Footer />
+      </Router>
     </div>
   );
 }
